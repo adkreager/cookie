f339d187edef128d1db54add984641a5cf93c1d5
Alexis <alexisdkreager@gmail.com>
2020-06-23 10:03:11 -0500

    /login adds cookie name "Alexis", /hello displays cookie name

--- index.js
+++ index.js
@@ -2,8 +2,17 @@ const express = require('express')
 const cookieParser = require('cookie-parser')
 const app = express()
 const port = 3000
-//app.use(express.json())
+
 app.use(cookieParser())
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+
+app.get('/login', (req, res) => {
+    res.cookie('name', "Alexis")
+    res.send("The cookie was set")
+})
+
+app.get('/hello', (req, res) => {
+    res.send(`Welcome ${req.cookies.name}!`)
+})
 
